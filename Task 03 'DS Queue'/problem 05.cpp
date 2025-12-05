1cdefc90a60be070f1f901eda445028d1a372a84
Ayaat <ayatemgin5@gmail.com>
2025-12-05 02:08:36 +0200

    bal3ab tany

--- Task 03 'DS Queue'/problem 05.cpp
+++ Task 03 'DS Queue'/problem 05.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         queue<int> q;
-        int cnt = 0 , idx = 0;
-        for (int s : students)
-            q.push(s);
-        while (!q.empty() && idx < students.size()) {
-            int x = q.front();
-            q.pop();
-            if (x == sandwiches[idx])
-                cnt = 0,idx++;
-            else {
-                q.push(x);
-                cnt++;
-                if (cnt == q.size()) break;
+        int cnt1 = 0,cnt0 = 0;
+        for (auto i : students)
+            if(i) cnt1++;
+        cnt0 = students.size() - cnt1;    
+        for(auto i : sandwiches){
+            if(i){
+                if(!cnt1) break;
+                cnt1--;
+            }
+            else{
+                if(!cnt0) break;
+                cnt0--;
             }
         }
-        return q.size();
+        return cnt0 + cnt1 ;
     }
 };
